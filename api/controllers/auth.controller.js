b13a082bd3924ce34925a6b1241558980e143278
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-04-03 01:15:50 +0700

    DONE:testing login route and return user data

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,10 +3,11 @@ import bcrypt from "bcrypt"
 
 const register = async (req, res) => {
     try {
-        const hash = bcrypt.hashSync(req.body.password, 10)
+        const { password } = req.body
+        const hashedPassword = bcrypt.hashSync(password, 5)
         const user = new User({
             ...req.body,
-            password: hash
+            password: hashedPassword
         })
         const result = await user.save()
         res.status(201).json({ status: "success", message: "berhasil dibuat" })
@@ -17,8 +18,27 @@ const register = async (req, res) => {
     }
 }
 
-const login = (req, res) => {
-    res.send('login')
+const login = async (req, res) => {
+    try {
+        // Take password in DB and store it into variable
+        const { username } = req.body
+        const user = await User.findOne({ username })
+
+        // check user
+        if (!user) return res.status(404).json({ status: "error", message: "User not found" })
+
+        // comparing password in DB with client
+        const isCorrect = bcrypt.compareSync(req.body.password, user.password)
+        if (!isCorrect) return res.status(400).json({ status: "error", message: "wrong password or username" })
+
+        // if everything OK show user
+        const { password, ...info } = user._doc
+        res.status(200).send(info)
+    }
+    catch (err) {
+        // console.log("Error: ", err)
+        res.status(500).json({ status: "error", message: err.message })
+    }
 }
 
 export { register, login }
