1af4c1240b2bca151be4974a6c5452f24419a30a
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-04-02 23:20:21 +0700

    DONE:adding bcrypt to hash password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,19 @@
-import {User} from '../models/user.model.js'
+import { User } from '../models/user.model.js'
+import bcrypt from "bcrypt"
 
 const register = async (req, res) => {
-    try{
-        const user = new User(req.body)
+    try {
+        const hash = bcrypt.hashSync(req.body.password, 10)
+        const user = new User({
+            ...req.body,
+            password: hash
+        })
         const result = await user.save()
-        res.status(201).json({status: "success", message: "berhasil dibuat"})
+        res.status(201).json({ status: "success", message: "berhasil dibuat" })
         console.log(result)
-    }catch(err){
+    } catch (err) {
         console.log('Error: ', err.message)
-        res.status(500).json({status: "error", message: err.message})
+        res.status(500).json({ status: "error", message: err.message })
     }
 }
 
@@ -16,4 +21,4 @@ const login = (req, res) => {
     res.send('login')
 }
 
-export {register, login}
+export { register, login }
